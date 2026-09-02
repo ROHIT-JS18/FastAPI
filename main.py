a0bbc79017424da867d4ae07022da379ddb72221
Rohit Joshi <rjoshi.1806@gmail.com>
2026-09-02 18:48:12 +0530

    Added HTTP methods

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+from fastapi import FastAPI
+app = FastAPI()
+
+# Define a route and a function (e.g., a "Hello World" endpoint):
+# python
+
+def load_data():
+    with open('patient.json' , 'r') as f:
+        data = json.load(f)
+    return data
+
+@app.get("/")
+def hello():
+    return {"message": "Patient Management system API"}
+
+@app.get("/about")
+def about():
+    return {'message':'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
