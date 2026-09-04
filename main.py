b8a1aba7d267df44a7a43a059eee3b2f8a1900e2
Rohit Joshi <rjoshi.1806@gmail.com>
2026-09-04 21:06:57 +0530

    Added Path and Query

--- main.py
+++ main.py
@@ -1,25 +1,67 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException , Query
+
 app = FastAPI()
 
-# Define a route and a function (e.g., a "Hello World" endpoint):
-# python
 
 def load_data():
-    with open('patient.json' , 'r') as f:
+    with open("patient.json", "r") as f:
         data = json.load(f)
     return data
 
+
 @app.get("/")
 def hello():
-    return {"message": "Patient Management system API"}
+    return {
+        "message": "Patient Management System API"
+    }
+
 
 @app.get("/about")
 def about():
-    return {'message':'A fully functional API to manage your patient records'}
+    return {
+        "message": "A fully functional API to manage your patient records"
+    }
 
-@app.get('/view')
+
+@app.get("/view")
 def view():
     data = load_data()
-
     return data
+
+
+@app.get("/patient/{patient_id}")
+def view_patient(
+    patient_id: str = Path(
+        ...,
+        description="ID of the patient in the DB",
+        example="P001"
+    )
+):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+
+    raise HTTPException(
+        status_code=404,
+        detail="Patient not found"
+    )
+
+@app.get('/sort')
+def sort_patients(sort_by:str = Query(...,description="Sort on basis of height , weight and BMI"),order:str=Query('asc',description='sort in asc or desc order')):
+    valid_fields =['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail="Invalid field select from {valid_fields}")
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail="Invalid field select from asc or desc")
+
+    data = load_data()
+    sort_order = True if order=="desc" else False
+
+    sorted_data = sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+
+    return sort_order , sorted_data
+
