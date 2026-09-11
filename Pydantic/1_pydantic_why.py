8723b03737e856d2768762dd293d2f23093f495f
Rohit Joshi <rjoshi.1806@gmail.com>
2026-09-11 19:18:16 +0530

    Changes Updates

--- Pydantic/1_pydantic_why.py
+++ Pydantic/1_pydantic_why.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional, Annotated
-
+from typing import List, Dict, Optional, Annotated 
+'''
+Define a Model: Creating a BaseModel class to set the data schema.
+Instantiate the Model: Creating an object from raw input (like a dictionary), where validation occurs automatically.
+Function Integration: Passing the validated object to functions, ensuring type safety and cleaner, more reliable code.
+'''
 class Patient(BaseModel):
 
     name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Give the name of the patient in less than 50 chars', examples=['Nitish', 'Amit'])]
@@ -12,7 +16,6 @@ class Patient(BaseModel):
     allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact_details: Dict[str, str]
 
-
 def update_patient_data(patient: Patient):
 
     print(patient.name)
@@ -21,8 +24,6 @@ def update_patient_data(patient: Patient):
     print(patient.married)
     print('updated')
 
-patient_info = {'name':'nitish', 'email':'abc@gmail.com', 'linkedin_url':'http://linkedin.com/1322', 'age': '30', 'weight': 75.2,'contact_details':{'phone':'2353462'}}
-
-patient1 = Patient(**patient_info)
-
-update_patient_data(patient1)
+patient_info ={'name':'John','age':20, 'weight':75.2,'married':True,'allergies':['pollen','dust'], 'contact_details':{'email':'abc@gmail.com','phone':'234567891'}}
+validated_patient = Patient(**patient_info)
+update_patient_data(validated_patient)
